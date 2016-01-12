d45cb451980ef0cd0110f7585edcded6c8477168
Heaton Cai <heaton.cai@gmail.com>
2016-01-12 18:27:08 +0800

    add terrible codes

---
+++ src/main/java/com/thoughtworks/troops/Adult.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.troops;
+
+public class Adult extends Member {
+
+	public Adult(String name, int position, int type) {
+		super(name, position, type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/com/thoughtworks/troops/MemberList.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.troops;
+
+import java.util.ArrayList;
+
+public class MemberList {
+
+	protected ArrayList<Member> memberList = null;
+
+	public MemberList() {
+		super();
+	}
+
+}
